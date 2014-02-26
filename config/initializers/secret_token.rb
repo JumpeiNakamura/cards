7358e5f2beee7173ef144fdce1f41ba00efaf8a1
Jeff Cohen <jeff@starterleague.com>
2014-02-26 10:38:28 -0600

    Generate the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cards::Application.config.secret_key_base = 'd191b792d1c232972398166f8641c583f358a0694534ba2808754545a0c4e5fdc0bc5fc81c8f7627779812536f5d95dd3e7c8514063d2176314e80bcb7cdadbc'
